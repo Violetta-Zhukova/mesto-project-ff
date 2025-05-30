4ee1e32f0ef01d1a5d9a43ad4af43207ab3a7954
Violetta Zhukova <puzanova.violetta@gmail.com>
2025-05-30 20:48:40 +0300

    bugs fixed

--- scripts/index.js
+++ scripts/index.js
@@ -9,19 +9,20 @@ function addCard(imgValue, titleValue, deleteCard) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const deleteButton = cardElement.querySelector(".card__delete-button");
+  const cardImage = cardElement.querySelector(".card__image");
 
-  cardElement.querySelector(".card__image").src = imgValue;
-  cardElement.querySelector(".card__image").alt = titleValue;
+  cardImage.src = imgValue;
+  cardImage.alt = titleValue;
   cardElement.querySelector(".card__title").textContent = titleValue;
 
   deleteButton.addEventListener("click", function () {
     deleteCard(cardElement);
   });
 
-  cardList.append(cardElement);
   return cardElement;
 }
 
 initialCards.forEach(function (element) {
-  addCard(element.link, element.name, deleteCard);
+  const card = addCard(element.link, element.name, deleteCard);
+  cardList.append(card);
 });
